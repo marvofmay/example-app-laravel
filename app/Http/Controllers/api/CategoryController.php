2ab7da150c1cb45f4c963baa217cfe22e147979f
Marcin <kontakt@internetowe-aplikacje.pl>
2022-02-26 01:31:19 +0100

    testy testy...

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
